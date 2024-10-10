d149c557a3d14e8c637ea1c6877b8dbbe6b39e4f
Jeffrey-mu <1115378579@qq.com>
2024-10-10 14:11:47 +0800

    chore: 删除多余代码

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 import express from "express"
 import cors from "cors"
-
 import archive from './src/routes/archive.js'
-let config = {
-  gamePath: '',
-  saveDir: '',
-  useSave: ''
-}
+
 const app = express()
 const port = 9001
 app.use(express.static("public"))
@@ -18,10 +13,6 @@ app.use('/archive', archive)
 
 app.get('/welcome', (req, res) => {
   res.send({ data: '欢迎来到黑悟空服务！' })
-})
-
-app.get('/getLocalConfig', (req, res) => {
-  res.send(config)
 })
 
 app.listen(port, () => {
